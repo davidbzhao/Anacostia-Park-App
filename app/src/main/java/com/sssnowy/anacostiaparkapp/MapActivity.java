6f8f960e9c6825bc4b58a57297c78e7691fb98d5
Snowyblack58 <davidzhao058@gmail.com>
2016-02-10 10:07:52 -0500

    Tested Google Maps Polygons and GroundMaps

--- app/src/main/java/com/sssnowy/anacostiaparkapp/MapActivity.java
+++ app/src/main/java/com/sssnowy/anacostiaparkapp/MapActivity.java
@@ -1,14 +1,24 @@
 package com.sssnowy.anacostiaparkapp;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,8 +49,34 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tjlatlng = new LatLng(38.817328, -77.169412);
+        mMap.addMarker(new MarkerOptions().position(tjlatlng).title("Marker in TJ"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tjlatlng, 15f));
+
+//        BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.drawable.map);
+//        LatLngBounds latLngBounds = new LatLngBounds(new LatLng(38.817328, -77.169412), new LatLng(38.818726, -77.167678));
+//        GroundOverlay groundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+//                .image(image)
+//                .positionFromBounds(latLngBounds)
+//                .transparency(0.5f));
+
+        mMap.addPolygon(new PolygonOptions().add(
+                new LatLng(38.819745, -77.170083),
+                new LatLng(38.819711, -77.168661),
+                new LatLng(38.819189, -77.166832),
+                new LatLng(38.816568, -77.168307),
+                new LatLng(38.817754, -77.170129))
+                .strokeColor(ContextCompat.getColor(this, R.color.zoneBorder))
+                .strokeWidth(4f)
+                .fillColor(ContextCompat.getColor(this, R.color.zoneFill)));
+
+        mMap.addPolygon(new PolygonOptions().add(
+                new LatLng(38.820163, -77.169949),
+                new LatLng(38.819164, -77.166381),
+                new LatLng(38.822182, -77.165636),
+                new LatLng(38.822169, -77.169965))
+                .strokeColor(ContextCompat.getColor(this, R.color.zoneBorder))
+                .strokeWidth(4f)
+                .fillColor(ContextCompat.getColor(this, R.color.zoneFill)));
     }
 }
