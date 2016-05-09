97985705b85797820e6e01d182b46d47da7298a7
Snowyblack58 <davidzhao058@gmail.com>
2016-05-09 09:32:24 -0400

    Parse in event information in event information activity

--- app/src/main/java/com/sssnowy/anacostiaparkapp/EventInformationActivity.java
+++ app/src/main/java/com/sssnowy/anacostiaparkapp/EventInformationActivity.java
@@ -1,13 +1,19 @@
 package com.sssnowy.anacostiaparkapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Html;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 public class EventInformationActivity extends Activity {
 
@@ -19,13 +25,64 @@ public class EventInformationActivity extends Activity {
 
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
+        Log.e("mylogs", url);
 
-        Document document = Jsoup.parse(url);
-        document.getElementById("submiddle");
+        AsyncTask<String, String, String> displayEventInformationAsyncTask = new AsyncTask<String, String, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                String url = params[0];
+                Document document = null;
+                try {
+                    document = Jsoup.connect(url).get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-        final WebView webView = (WebView)findViewById(R.id.eventWebView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(url);
+                String title = document.title();
+                title = title.replace("•","").substring(0, title.indexOf("|")).trim();
+                publishProgress(title);
+
+                for(Element element : document.getElementById("submiddle").getElementsByTag("img")){
+                    element.remove();
+                }
+                return document.getElementById("submiddle").toString();
+            }
+
+            @Override
+            protected void onProgressUpdate(String... values) {
+                super.onProgressUpdate(values);
+                getActionBar().setTitle(values[0]);
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Log.e("mylogs", s);
+
+                WebView webView = (WebView)findViewById(R.id.eventWebView);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.loadDataWithBaseURL(null, s, "text/html", "utf-8", null);
+            }
+        };
+
+        displayEventInformationAsyncTask.execute(url);
+//        Document document = null;
+//        try {
+//            document = Jsoup.connect(url).get();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Log.e("mylogs", String.valueOf(document == null));
+//
+//        Log.e("mylogs", document.toString());
+//        Log.e("mylogs", String.valueOf(document.getElementById("submiddle") == null));
+//        Log.e("mylogs", document.getElementById("submiddle").toString());;
+//        String html = Html.fromHtml(document.getElementById("submiddle").toString()).toString();
+
+//        final WebView webView = (WebView)findViewById(R.id.eventWebView);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl(url);
     }
 
 }
